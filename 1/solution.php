55957e91229ab612a4f4c37cbe37dc6522f71c8f
Jamie Bambrough <jamie.bambrough@doddgroup.com>
2025-12-02 16:34:56 +0000

    patch: star 1

---
+++ 1/solution.php
@@ -0,0 +1,43 @@
+<?php
+
+$import = file_get_contents('./input.txt');
+$lines = explode("\n", $import);
+
+$dial = 50;
+$password = 0;
+$count = 0;
+$maxLines = -1;
+echo "Starting... Dial is at $dial \n";
+
+
+foreach ($lines as $line) {
+    if (empty($line)) {
+        continue;
+    }
+    $direction = $line[0];
+    $amount = (int) substr($line, 1);
+    echo "Dial turned from $dial ";
+    switch ($direction) {
+        case "L":
+            $dial -= $amount;
+
+            break;
+        case "R":
+            $dial += $amount;
+
+            break;
+        default:
+            throw new Exception("No direction found");
+    }
+    while ($dial >= 100)
+        $dial -= 100;
+    while ($dial < 0)
+        $dial += 100;
+    if ($dial === 0)
+        $password++;
+    echo "in direction $direction by $amount to $dial\n";
+    $count++;
+    if ($count === $maxLines)
+        die;
+}
+echo "Counted zero position $password times";
--- 1/solution.ts
+++
@@ -1,5 +0,0 @@
-// import from input.txt
-import fs;
-const input = fs.readFileSync('input.txt', 'utf-8');
-const lines = input.split('\n');
-
